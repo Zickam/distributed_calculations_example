35b35c1d9096878bd4a385fd99a9a3b32c4c2d50
Almaz <Zickam@mail.ru>
2024-12-16 11:24:58 +0500

    1

--- test_calculations.py
+++ test_calculations.py
@@ -29,7 +29,7 @@ def testSynchronousCalc():
     time_spent = end_time - start_time
 
     print("Synchronous time:", time_spent, "seconds")
-    print("Synchronous output:", primes)
+    print("Synchronous output:", primes, "amount:", len(primes))
 
 
 async def testDistributedCalc():
@@ -58,7 +58,7 @@ async def testDistributedCalc():
     time_spent = end_time - start_time
 
     print("Distributed time:", time_spent, "seconds")
-    print("Distributed output:", primes)
+    print("Distributed output:", primes, "amount:", len(primes))
 
 
 def main():
